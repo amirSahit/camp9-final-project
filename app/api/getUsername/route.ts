60a115c3a0f4936db6ac37e68b8e40de9687ca57
Amir <amir.sahit@gmail.com>
2023-07-28 14:44:59 +0200

    trying to make findUser smaller

--- app/api/getUsername/route.ts
+++ app/api/getUsername/route.ts
@@ -1,11 +1,13 @@
-import { db } from '@/libs/db';
+import { PrismaClient } from '@prisma/client';
 import { NextResponse } from 'next/server';
+
+const prisma = new PrismaClient();
 
 export async function GET(request: Request) {
   const { searchParams } = new URL(request.url);
   const id = searchParams.get('id')!;
 
-  const user = await db.user.findUnique({
+  const user = await prisma.user.findUnique({
     where: {
       id: +id,
     },
