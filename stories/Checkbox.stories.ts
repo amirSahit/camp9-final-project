8a0aa73c6b2f033eff13374de3d55a367467ee65
Amir <amir.sahit@gmail.com>
2023-07-28 13:42:20 +0200

    fixed import of checkboxInbox in stories

--- stories/Checkbox.stories.ts
+++ stories/Checkbox.stories.ts
@@ -1,4 +1,4 @@
-import { Checkboxinput } from '@/components/CheckboxInput';
+import { Checkboxinput } from '../components/Checkboxinput';
 import type { Meta, StoryObj } from '@storybook/react';
 
 const meta: Meta<typeof Checkboxinput> = {
