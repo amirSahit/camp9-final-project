72d9f8191aa8200446016b67edba237f01175887
Amir <amir.sahit@gmail.com>
2023-07-28 13:38:54 +0200

    fixed Consensus Controller

--- components/ConsensusController.tsx
+++ components/ConsensusController.tsx
@@ -11,58 +11,45 @@ function ConsensusController() {
 
   const inputRef = useRef<HTMLInputElement>(null);
 
-  // useEffect(() => {
-  //   let quorum = +getValues().quorum;
-  //   setValueController(quorum);
-  //   if (inputRef.current) {
-  //     inputRef.current.style.setProperty(
-  //       'background-size',
-  //       `${valueController}%`
-  //     );
-  //   }
-  // }, [valueController]);
-
-  function handleInput(e: React.ChangeEvent<HTMLInputElement>) {
+  useEffect(() => {
+    setValueController(+getValues().quorum);
     if (inputRef.current) {
       inputRef.current.style.setProperty(
         'background-size',
-        `${e.target.value}%`
+        `${valueController}%`
       );
-      setValue('quorum', e.target.value);
-      setValueController(parseInt(e.target.value));
     }
+  }, [valueController]);
 
-    return (
-      <div className="w-full">
-        <div className="flex flex-row justify-between items-center">
-          <input
-            type="range"
-            min="0"
-            max="100"
-            step="10"
-            list="valuesConesnusController"
-            value={+getValues().quorum}
-            {...register('quorum')}
-            id="rangeValue"
-            onChange={e => {
-              // setValue('quorum', e.target.value);
-              // setValueController(parseInt(e.target.value));
-              handleInput(e);
-            }}
-            ref={inputRef}
-          />
+  return (
+    <div className="w-full">
+      <div className="flex flex-row justify-between items-center">
+        <input
+          type="range"
+          min="0"
+          max="100"
+          step="10"
+          list="valuesConesnusController"
+          value={+getValues().quorum}
+          {...register('quorum')}
+          id="rangeValue"
+          onChange={e => {
+            setValue('quorum', e.target.value);
+            setValueController(parseInt(e.target.value));
+          }}
+          ref={inputRef}
+        />
 
-          <p>
-            <output id="outputValue">{valueController}%</output>
-          </p>
-        </div>
-        <datalist id="valuesConesnusController">
-          <option value="0%" label="0%" />
-          <option value="100%" label="100%" />
-        </datalist>
+        <p>
+          <output id="outputValue">{valueController}%</output>
+        </p>
       </div>
-    );
-  }
+      <datalist id="valuesConesnusController">
+        <option value="0%" label="0%" />
+        <option value="100%" label="100%" />
+      </datalist>
+    </div>
+  );
 }
 
 export default ConsensusController;
